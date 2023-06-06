139e6311b1b8cbad0719fed238ada8540d83fb47
Spoorthi K <102209212+Spoorthi00@users.noreply.github.com>
2023-06-06 12:28:03 +0530

    variables.py

---
+++ variables.py
@@ -0,0 +1,22 @@
+from pickle import TRUE
+
+
+a = "Harry" #String
+b = 345 #Integer
+c = 45.32  #floating Point
+d = True #Boolean
+e = None  #Nothing
+
+#Printing the Variables
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+#Printing the Type of Variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
