6617715d48551a56f2a3d8891210f7b2cb839ed7
Spoorthi K <102209212+Spoorthi00@users.noreply.github.com>
2023-06-06 12:30:56 +0530

    tempcoderunnerfile.py

---
+++ tempcoderunner.py
@@ -0,0 +1,27 @@
+a = 3
+b = 4
+
+#Arithematic Operator
+print("The value of 3+4 is", 3+4)
+print("The value of 3-4 is", 3-4)
+print("The value of 3*4 is", 3*4)
+print("The value of 3/4 is", 3/4)
+
+
+
+#Assignment Operators
+a = 34
+a += 2  #adding the total
+print(a)
+
+b = 34
+b -= 2  #Substract the total
+print(b)
+
+c = 34
+c *= 2  #Multiply the total
+print(c)
+
+d = 34
+d /= 2  #Division the total
+print(d)
