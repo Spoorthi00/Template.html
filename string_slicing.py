67fdd45db38b2e44e110e378bada36a95c72a118
Spoorthi K <102209212+Spoorthi00@users.noreply.github.com>
2023-06-06 12:29:16 +0530

    string_slicing.py

---
+++ string_slicing.py
@@ -0,0 +1,49 @@
+'''
+Greeting = "Good Morning, "
+Name = "Spoorthi"
+print(Greeting + Name)
+   
+
+   #OR
+
+Greeting = "Good Morning, "
+Name = "Spoorthi"
+c = Greeting + Name
+print(c)
+'''
+
+#to know the length  
+'''
+ Name = "Spoorthi"
+print(Name[2])
+'''
+
+#string Slicing (cut)
+'''
+Name = "Vinoda"
+print(Name[0:5]) # is same as [:5]
+print(Name[1:]) # is same as [1:6]
+'''
+
+#Negative Slice
+'''
+   N    a    g    e    n   d    r    a
+   0    1    2    3    4   5    6    7
+  -8   -7   -6   -5   -4  -3   -2   -1   #negative Slice
+  
+  
+
+Name = "Nagendra"
+c = Name[-4:-1] # is same as [1:4]
+print(c)
+'''
+
+
+#Slice Skip
+'''
+Name = "Nagendraisgood"
+d = Name[0:6:3]
+e = Name[0::2]
+print(d)
+print(e)
+'''
