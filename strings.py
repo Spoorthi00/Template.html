215649998debcdde89aa55b97b80038cb28974f4
Spoorthi K <102209212+Spoorthi00@users.noreply.github.com>
2023-06-06 12:29:43 +0530

    strings.py

---
+++ strings.py
@@ -0,0 +1,19 @@
+# Double quoted
+
+b = "Spoo"
+print(b)
+
+
+#single quoted
+
+c ='Pooja'
+print(c)
+
+#tripple Quoted
+
+d = '''Vinoda'''
+print(d)
+
+
+e = '''"Pooja and spoorthi" are daugther's of Vinoda'''
+print(e)
