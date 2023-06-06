23b69d940a4704aaa25f052c920084e45f1e63ae
Spoorthi K <102209212+Spoorthi00@users.noreply.github.com>
2023-06-06 12:27:03 +0530

    opeartors.py

---
+++ opeartors.py
@@ -0,0 +1,57 @@
+a = 3
+b = 4
+
+#Arithematic Operator
+print("The value of 3+4 is", 3+4)
+print("The value of 3-4 is", 3-4)
+print("The value of 3*4 is", 3*4)
+print("The value of 3/4 is", 3/4)
+
+
+
+#Assignment Operators
+a = 34
+a += 2  #adding the total
+print(a)
+
+b = 34
+b -= 2  #Substract the total
+print(b)
+
+c = 34
+c *= 2  #Multiply the total
+print(c)
+
+d = 34
+d /= 2  #Division the total
+print(d)
+
+
+#Comparison Operator  (Boolean Answer we get)
+
+b = (14>7)   #greater
+print(b)  
+
+c = (4<7)  #lesser
+print(c)
+
+d = (14>=7) #greater then equal to
+print(d)
+
+e = (14<=7)  #lesser then equal to
+print(e)
+
+f = (14==7) #equal equal to
+print(f)
+
+g = (14!=7) #Not equal to
+print(g)
+
+
+#Logical operator
+
+bool1 = True
+bool2 = False
+print("The value of bool1 and bool2 is", (bool1 and bool2)) #multiply
+print("The value of bool1 and bool2 is", (bool1 or bool2))  #add
+print("The value of not bool2 is", (not bool2))   #opposite
