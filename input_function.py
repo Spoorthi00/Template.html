2d4aeec665ceb3a27d29dcddcffd85ccf40cdc20
Spoorthi K <102209212+Spoorthi00@users.noreply.github.com>
2023-06-06 12:26:36 +0530

    input_function.py

---
+++ input_function.py
@@ -0,0 +1,16 @@
+#Input
+
+# a = input("Enter Your Name")
+# print(a)
+
+
+
+
+
+
+#square input
+
+# a = input("Enter the value to do the square:")
+# a = int(a)
+# square = (a*a)
+# print("the squared value of given number is", square)
